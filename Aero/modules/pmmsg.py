80dad674d00cbfce0366356d1e60d99e1ab9a83f
AerodynamicV1Botz <104615877+AerodynamicV1Botz@users.noreply.github.com>
2022-06-16 22:43:51 +0530

    Create pmmsg.py

---
+++ Aero/modules/pmmsg.py
@@ -0,0 +1,15 @@
+# Copyright (©️) @M8N_OFFICIAL
+# By : Pavan Magar
+
+from pyrogram import Client
+from m8n.tgcalls import client as USER
+from pyrogram import filters
+from pyrogram.types import Chat, Message, User
+from m8n.config import (
+    BOT_USERNAME,
+)
+
+@USER.on_message(filters.text & filters.private & ~filters.me & ~filters.bot)
+async def pmPermit(client: USER, message: Message):
+  await USER.send_message(message.chat.id,"Hey 👋 I am the assistant of music bot")
+  return
